e6437e8ac85b54321830bdec55f10e9b3a7d3554
heitorleonny <heitorleonnyyt1234@gmail.com>
2023-08-19 15:25:24 -0300

    tipos variaveis

---
+++ TIPOS-VARIAVEIS/src/TiposVariaveis.java
@@ -0,0 +1,11 @@
+public class TiposVariaveis {
+    public static void main(String[] args) {
+        int numero = 5;
+        final double VALOR_PI = 3.14;
+        numero = 10;
+   
+
+        System.out.println(numero);
+        System.out.println(VALOR_PI);
+    }
+}
