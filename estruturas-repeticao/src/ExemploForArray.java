190a292c567bf272cacd6eda40035b4ed97c0fc6
heitor <heitorleonnyyt1234@gmail.com>
2023-08-21 23:49:37 -0300

    Adicionando aulas sobre estruturas condicionais e de repetição

---
+++ estruturas-repeticao/src/ExemploForArray.java
@@ -0,0 +1,10 @@
+public class ExemploForArray {
+    public static void main(String[] args) {
+        String alunos[] = {"FELIPE", "JONAS", "MARCOS"};
+
+        for (String aluno : alunos){
+            System.out.println("O nome do aluno é: " + aluno);
+            
+        }
+    }
+}
